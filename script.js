bc53af49ca1de3e183311c98bc426bd84fb5992e
mayke <127307061+maykegirraldi@users.noreply.github.com>
2024-08-16 12:33:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script src="script.jts"></script>
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
